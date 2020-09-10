5357995139e6087e1e795e69fcfcb454c538bd81
Chris Ritzo <critzo@measurementlab.net>
2020-09-10 10:54:10 -0400

    removed all WIP stuff

--- statistics/queries/WIP/get_continent_codes.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  client.geo.continent_code AS continent_code
-FROM `ndt.unified_downloads`
-WHERE test_date >= '2020-01-01'
-AND client.geo.continent_code IS NOT NULL AND client.geo.continent_code != ''
-GROUP BY continent_code
-ORDER BY continent_code
--- statistics/queries/WIP/get_continent_country_codes.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  client.geo.continent_code AS continent_code,
-  client.geo.country_code AS country_code
-FROM `ndt.unified_downloads`
-WHERE test_date >= '2020-01-01'
-AND client.geo.continent_code IS NOT NULL AND client.geo.continent_code != ''
-AND client.geo.country_code IS NOT NULL AND client.geo.country_code != ''
-GROUP BY continent_code, country_code
-ORDER BY continent_code, country_code
