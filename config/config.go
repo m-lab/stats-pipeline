893c23bcfda9f2d5918bf61c1f7b175a8f758063
Roberto D'Auria <roberto@measurementlab.net>
2020-11-17 11:27:49 +0100

    Add config/config.go.

---
+++ config/config.go
@@ -0,0 +1,20 @@
+package config
+
+type Config struct {
+	Histograms map[string]HistogramConfig
+	Exports    map[string]ExportConfig
+}
+
+type HistogramConfig struct {
+	QueryFile string
+	Dataset   string
+	Table     string
+}
+
+type ExportConfig struct {
+	SourceTable string
+	QueryFile   string
+	OutputPath  string
+	ShardKey    string
+	BatchSize   int
+}
