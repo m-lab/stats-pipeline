d516ad7bdad668b0b71f348e532838ab21b1c438
Chris Ritzo <critzo@measurementlab.net>
2021-01-28 08:50:29 -0500

    change query per some, but not all of review comments.

--- statistics/queries/continent_histogram.sql
+++ statistics/queries/continent_histogram.sql
@@ -1,10 +1,10 @@
 WITH
-# Generate equal sized buckets in log-space between near 0 Mbps and ~1 Gbps+
+--Generate equal sized buckets in log-space between near 0 Mbps and ~1 Gbps+
 buckets AS (
   SELECT POW(10, x-.25) AS bucket_left, POW(10,x+.25) AS bucket_right
   FROM UNNEST(GENERATE_ARRAY(0, 3.5, .5)) AS x
 ),
-# Select the initial set of results
+--Select the initial set of results
 dl_per_location AS (
   SELECT
     date,
@@ -17,7 +17,7 @@ dl_per_location AS (
   WHERE date BETWEEN @startdate AND @enddate
   AND a.MeanThroughputMbps != 0
 ),
-# With good locations and valid IPs
+--Filter for only tests With good locations and valid IPs
 dl_per_location_cleaned AS (
   SELECT * FROM dl_per_location
   WHERE
@@ -25,8 +25,7 @@ dl_per_location_cleaned AS (
     AND continent_code != ""
     AND ip IS NOT NULL
 ),
-
-# Gather rows per day and ip
+--Gather rows per day and ip
 dl_rows_perip_perday AS (
   SELECT
     date,
@@ -46,44 +45,42 @@ dl_rows_perip_perday AS (
     ) AS stats
   FROM dl_per_location_cleaned
   GROUP BY date, continent_code, ip
-  --ORDER BY ip, date
 ),
-
+--Use a prime number larger than the number of total rows in an aggregation to select a random row 
 dl_random_perip_perday AS (
   SELECT
     date,
     continent_code,
     ip,
     ARRAY_LENGTH(members) AS tests,
-    # Use a prime number larger than the number of total rows in an aggregation to select a random row 
     members[SAFE_OFFSET(MOD(511232941,ARRAY_LENGTH(members)))] AS random1,
     members[SAFE_OFFSET(MOD(906686609,ARRAY_LENGTH(members)))] AS random2,
     stats
   FROM dl_rows_perip_perday
 ),
-
-# Calculate log average per day from random samples
+--Calculate log average per day from random samples
 dl_stats_per_day AS (
   SELECT
     date, continent_code,
-    ROUND(POW(10,AVG(Safe.LOG10(stats.download_MED))),3) AS dl_day_log_avg_median,
-    ROUND(POW(10,AVG(Safe.LOG10(random1.MinRtt))),3) AS dl_min_rtt_day_log_avg,
+    COUNT(*) AS dl_samples_day,
     ROUND(POW(10,AVG(Safe.LOG10(random1.mbps))),3) AS dl_day_log_avg_random1,
-    ROUND(POW(10,AVG(Safe.LOG10(random2.mbps))),3) AS dl_day_log_avg_random2
+    ROUND(POW(10,AVG(Safe.LOG10(random2.mbps))),3) AS dl_day_log_avg_random2,
+    ROUND(POW(10,AVG(Safe.LOG10(random1.MinRtt))),3) AS dl_min_rtt_day_log_avg_random1,
+    ROUND(POW(10,AVG(Safe.LOG10(random2.MinRtt))),3) AS dl_min_rtt_day_log_avg_random2
   FROM dl_random_perip_perday
   GROUP BY continent_code, date
-  # ORDER BY continent_code, date
 ),
-# Count the samples that fall into each bucket and get frequencies
+--Count the samples that fall into each bucket and get frequencies
 dl_histogram AS (
   SELECT
     date,
     continent_code,
-    CASE WHEN bucket_left = 0.31622776601683794 THEN 0
+    --Set the lowest bucket's min to zero, so all tests below the generated min of the lowest bin are included. 
+    CASE WHEN bucket_left = 0.5623413251903491 THEN 0
     ELSE bucket_left END AS bucket_min,
     bucket_right AS bucket_max,
-    COUNTIF(stats.download_MED < bucket_right AND stats.download_MED >= bucket_left) AS dl_samples_bucket,
-    ROUND(COUNTIF(stats.download_MED < bucket_right AND stats.download_MED >= bucket_left) / COUNT(*), 3) AS dl_frac_bucket
+    COUNTIF(random1.mbps < bucket_right AND random1.mbps >= bucket_left) AS dl_samples_bucket,
+    ROUND(COUNTIF(random1.mbps < bucket_right AND random1.mbps >= bucket_left) / COUNT(*), 3) AS dl_frac_bucket
   FROM dl_random_perip_perday CROSS JOIN buckets
   GROUP BY
     date,
@@ -91,8 +88,8 @@ dl_histogram AS (
     bucket_min,
     bucket_max
 ),
-# Repeat for Upload tests
-# Select the initial set of upload results
+--Repeat for Upload tests
+--Select the initial set of upload results
 ul_per_location AS (
   SELECT
     date,
@@ -105,15 +102,14 @@ ul_per_location AS (
   WHERE date BETWEEN @startdate AND @enddate
   AND a.MeanThroughputMbps != 0
 ),
-# With good locations and valid IPs
+--With good locations and valid IPs
 ul_per_location_cleaned AS (
   SELECT * FROM ul_per_location
   WHERE
     continent_code IS NOT NULL AND continent_code != ""
     AND ip IS NOT NULL
 ),
-
-# Gather rows per day and ip
+--Gather rows per day and ip
 ul_rows_perip_perday AS (
   SELECT
     date,
@@ -132,53 +128,55 @@ ul_rows_perip_perday AS (
     ) AS stats
   FROM ul_per_location_cleaned
   GROUP BY date, continent_code, ip
-  --ORDER BY ip, date
 ),
-
+--Use a prime number larger than the number of total rows in an aggregation to select a random row 
 ul_random_perip_perday AS (
   SELECT
     date,
     continent_code,
     ip,
     ARRAY_LENGTH(members) AS tests,
-    # Use a prime number larger than the number of total rows in an aggregation to select a random row 
     members[SAFE_OFFSET(MOD(511232941,ARRAY_LENGTH(members)))] AS random1,
     members[SAFE_OFFSET(MOD(906686609,ARRAY_LENGTH(members)))] AS random2,
     stats
   FROM ul_rows_perip_perday
 ),
-# Calculate log average per day from random samples
+--Calculate log average per day from random samples
 ul_stats_per_day AS (
   SELECT
-    continent_code, date, 
-    ROUND(POW(10,AVG(Safe.LOG10(stats.upload_MED))),3) AS ul_day_log_avg_median,
-    ROUND(POW(10,AVG(Safe.LOG10(random1.MinRtt))),3) AS ul_min_rtt_day_log_avg,
+    continent_code, date,
+    COUNT(*) AS ul_samples_day,
     ROUND(POW(10,AVG(Safe.LOG10(random1.mbps))),3) AS ul_day_log_avg_random1,
     ROUND(POW(10,AVG(Safe.LOG10(random2.mbps))),3) AS ul_day_log_avg_random2,
+    ROUND(POW(10,AVG(Safe.LOG10(random1.MinRtt))),3) AS ul_min_rtt_day_log_avg_random1,
+    ROUND(POW(10,AVG(Safe.LOG10(random2.MinRtt))),3) AS ul_min_rtt_day_log_avg_random2
   FROM ul_random_perip_perday
   GROUP BY continent_code, date
-  # ORDER BY continent_code, date
 ),
-# Count the samples that fall into each bucket and get frequencies
+--Count the samples that fall into each bucket and get frequencies
 ul_histogram AS (
   SELECT
     date,
     continent_code,
-    CASE WHEN bucket_left = 0.31622776601683794 THEN 0
+    CASE WHEN bucket_left = 0.5623413251903491 THEN 0
     ELSE bucket_left END AS bucket_min,
     bucket_right AS bucket_max,
-    COUNTIF(stats.upload_MED < bucket_right AND stats.upload_MED >= bucket_left) AS ul_samples_bucket,
-    ROUND(COUNTIF(stats.upload_MED < bucket_right AND stats.upload_MED >= bucket_left) / COUNT(*),3) AS ul_frac_bucket
+    COUNTIF(random1.mbps < bucket_right AND random1.mbps >= bucket_left) AS ul_samples_bucket,
+    ROUND(COUNTIF(random1.mbps < bucket_right AND random1.mbps >= bucket_left) / COUNT(*), 3) AS ul_frac_bucket
   FROM ul_random_perip_perday CROSS JOIN buckets
   GROUP BY
     date,
     continent_code,
     bucket_min,
     bucket_max
+),
+--Gather final result set
+results AS (
+  SELECT *, MOD(ABS(FARM_FINGERPRINT(continent_code)), 1000) as shard FROM dl_histogram
+  JOIN ul_histogram USING (date, continent_code, bucket_min, bucket_max)
+  JOIN dl_stats_per_day USING (date, continent_code)
+  JOIN ul_stats_per_day USING (date, continent_code)
 )
-# Show the results
-SELECT *, MOD(ABS(FARM_FINGERPRINT(continent_code)), 1000) as shard FROM dl_histogram
-JOIN ul_histogram USING (date, continent_code, bucket_min, bucket_max)
-JOIN dl_stats_per_day USING (date, continent_code)
-JOIN ul_stats_per_day USING (date, continent_code)
+--Show the results
+SELECT * EXCEPT(shard) FROM results
 ORDER BY date, continent_code, bucket_min
