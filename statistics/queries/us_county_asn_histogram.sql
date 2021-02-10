5751fa93d3440f93d44ff0ac2a5c46f5cea86165
Roberto D'Auria <roberto@measurementlab.net>
2021-02-10 18:14:19 +0100

    Add missing comma in counties ASN query.

--- statistics/queries/us_county_asn_histogram.sql
+++ statistics/queries/us_county_asn_histogram.sql
@@ -252,7 +252,7 @@ ul_histogram AS (
 --Gather final result set
 results AS (
   SELECT *, MOD(ABS(FARM_FINGERPRINT(state)), 4000) as shard FROM dl_histogram
-  JOIN ul_histogram USING (date, continent_code, country_code, state, GEOID
+  JOIN ul_histogram USING (date, continent_code, country_code, state, GEOID,
   asn, bucket_min, bucket_max)
   JOIN dl_stats_per_day USING (date, continent_code, country_code, state, GEOID,
   asn)
