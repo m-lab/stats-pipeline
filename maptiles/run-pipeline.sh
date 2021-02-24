9182f9c9757ae17ab94f5356dfac542b007b4c88
Roberto D'Auria <roberto@measurementlab.net>
2021-02-24 11:42:34 +0100

    Rename maptiles to stats-pipeline-runner

--- maptiles/run-pipeline.sh
+++ maptiles/run-pipeline.sh
@@ -15,5 +15,8 @@ if ! curl -X POST "http://stats-pipeline-service:8080/v0/pipeline?year=${year}";
 fi
 
 echo "Stats-pipeline completed successfully"
+# Note: this is disabled until the maptiles generation can run on multiple
+# years. Currently, 2020 is hardcoded and it would be pointless to regenerate
+# the maptiles every time the stats-pipeline runs.
 #export GCS_BUCKET=maptiles-${PROJECT}
 #make piecewise
