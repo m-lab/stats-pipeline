4bafa3182d0a3a24ae0b77e9f9f16b883d6e9d63
Roberto D'Auria <roberto@measurementlab.net>
2021-02-25 12:51:37 +0100

    Create CronJob to run stats-pipeline on GKE (#52)
    * Add CronJob to run the maptiles-runner
    
    * Fix cloudbuild.yaml
    
    * Rename runner.yaml
    
    * Update cloudbuild.yaml
    
    * Move concurrencyPolicy
    
    * Fix env variables
    
    * update deployment
    
    * Move concurrencyPolicy to the right place.
    
    * Set schedule from ENV variable.
    
    * Add restartPolicy: Never
    
    * Rename maptiles to stats-pipeline-runner
    
    * Rename -runner to -cronjob
    
    * Fix yaml name
    
    * Update run-pipeline
    
    * Remove extra \
    
    * Rename _TAG to _DOCKER_TAG
    
    * Rename PIPELINE_SCHEDULE to PIPELINE_CRON_SCHEDULE
    
    * Rename to .template
    
    * Add comments
    
    * Added comment

--- maptiles/run-pipeline.sh
+++ maptiles/run-pipeline.sh
@@ -9,11 +9,14 @@ PROJECT=${PROJECT:?Please provide project}
 # Start stats-pipeline for the current year
 year=$(date +%Y)
 
-if ! curl -X POST "http://stats-pipeline-service:8080/v0/pipeline?year=${year}"; then
+if ! curl -X POST "http://stats-pipeline-service:8080/v0/pipeline?year=${year}&step=all"; then
     echo "Stats-pipeline failed, please check the container logs."
     exit 1
 fi
 
-echo "Stats-pipeline completed successfully, generating maptiles..."
-export GCS_BUCKET=maptiles-${PROJECT}
-make piecewise
+echo "Stats-pipeline completed successfully"
+# Note: this is disabled until the maptiles generation can run on multiple
+# years. Currently, 2020 is hardcoded and it would be pointless to regenerate
+# the maptiles every time the stats-pipeline runs.
+#export GCS_BUCKET=maptiles-${PROJECT}
+#make piecewise
