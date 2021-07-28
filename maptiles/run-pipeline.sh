9a07840b8cc7138ccc4f0f6f7a7e2aaea8d2ba13
Roberto D'Auria <roberto@measurementlab.net>
2021-07-28 16:40:33 +0200

    Update run-pipeline.sh

--- maptiles/run-pipeline.sh
+++ maptiles/run-pipeline.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 PROJECT=${PROJECT:?Please provide project}
 
 # Start stats-pipeline for the past 2 days.
-start=$(date -d "2 days ago" +%Y-%m-%d)
+start=$(date --date "-2 days" +%Y-%m-%d)
 end=$(date +%Y-%m-%d)
 
 if ! curl -X POST "http://stats-pipeline-service:8080/v0/pipeline?start=${start}&end=${end}&step=all"; then
