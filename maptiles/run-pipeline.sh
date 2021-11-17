579faf11c7ce68a2acc3855b650c7085c182ce4e
Roberto D'Auria <roberto@measurementlab.net>
2021-11-17 14:17:59 +0100

    Add separate CronJob for annotation-export (#87)
    * Update run-pipeline.sh to take the endpoint as argument and the cronjob.
    
    * Update comments.
    
    * Add new annotation-export-cronjob cronjob.
    
    * Generate manifest for annotation-export and deploy it.
    
    * Pass the right endpoint in the annotation-export cronjob.
    
    * Fix template name in cloudbuild.yaml.
    
    * Use strategy.type: Recreate
    
    * Add \n
    
    * Remove PROJECT variable
    
    * Add note on how to run the annotation-export via kubectl

--- maptiles/run-pipeline.sh
+++ maptiles/run-pipeline.sh
@@ -4,18 +4,18 @@
 # generates updated maptiles.
 
 set -euxo pipefail
-PROJECT=${PROJECT:?Please provide project}
+ENDPOINT=${1?"Please provide the endpoint (hostname + port). Usage: $0 <endpoint>"}
 
-# Start stats-pipeline for the past 2 days.
+# Start the pipeline for the past 2 days.
 start=$(date -d "@$(( $(date +%s) - 86400 * 2 ))" +%Y-%m-%d)
 end=$(date +%Y-%m-%d)
 
-if ! curl -X POST "http://stats-pipeline-service:8080/v0/pipeline?start=${start}&end=${end}&step=all"; then
-    echo "Stats-pipeline failed, please check the container logs."
+if ! curl -X POST "http://$ENDPOINT/v0/pipeline?start=${start}&end=${end}&step=all"; then
+    echo "Running the pipeline failed, please check the container logs."
     exit 1
 fi
 
-echo "Stats-pipeline completed successfully"
+echo "The pipeline completed successfully"
 # Note: this is disabled until the maptiles generation can run on multiple
 # years. Currently, 2020 is hardcoded and it would be pointless to regenerate
 # the maptiles every time the stats-pipeline runs.
